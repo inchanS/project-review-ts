2fc4e23b232b2fc9c3e235fa9cafd2a6f626aff4
c0dject <creseeds@gmail.com>
2023-02-09 01:31:03 +0900

    Modify: validate Comment class => function - validate Comment fn         - if문을 이용한 에러 코드를 하나의 함수로 리팩토링하고 기존 클래스를 함수로 바꾸며 코드양을 줄임

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -30,44 +30,31 @@ const createComment = async (commentInfo: CommentDto): Promise<void> => {
   await CommentRepository.createComment(commentInfo);
 };
 
-// 덧글 수정, 삭제시 원본과 확인하는 클래스기반 함수
-class ValidateOriginComment {
-  userId: number;
-  commentId: number;
-  constructor(userId: number, commentId: number) {
-    this.userId = userId;
-    this.commentId = commentId;
-  }
+// DB에 있는 원 댓글을 찾고 없으면 반환
+const validateComment = async (userId: number, commentId: number) => {
+  const result = await CommentRepository.findOne({
+    loadRelationIds: true,
+    where: { id: commentId },
+  });
 
-  // DB에 있는 원 댓글을 찾고 없으면 반환
-  async originComment() {
-    const result = await CommentRepository.findOne({
-      loadRelationIds: true,
-      where: { id: this.commentId },
-    });
-
-    // typeorm softDelete 메소드를 사용하여 삭제한 경우, delete_at 컬럼의 값이 채워지는데,
-    // 이러한 데이터는 typeorm의 find 메소드로 검색시 알아서 null 처리해준다. (console 확인 결과)
-    // if (!result || result.deleted_at !== null) {
-    //   throw new Error(`ID_${this.commentId}_COMMENT_DOES_NOT_EXIST`);
-    // }
-
-    if (!result) {
-      throw new Error(`ID_${this.commentId}_COMMENT_DOES_NOT_EXIST`);
-    }
-    return result;
+  // typeorm softDelete 메소드를 사용하여 삭제한 경우, delete_at 컬럼의 값이 채워지는데,
+  // 이러한 데이터는 typeorm의 find 메소드로 검색시 알아서 null 처리해준다. (console 확인 결과)
+  // if (!result || result.deleted_at !== null) {
+  //   throw new Error(`ID_${this.commentId}_COMMENT_DOES_NOT_EXIST`);
+  // }
+  if (!result) {
+    throw new Error(`ID_${commentId}_COMMENT_DOES_NOT_EXIST`);
   }
 
   // 작성자 아이디가 다를 때 에러처리
-  async checkCommentWriter() {
-    const originComment = await this.originComment();
-    if (this.userId !== Number(originComment.user)) {
-      const error = new Error(`ONLY_THE_AUTHOR_CAN_EDIT`);
-      error.status = 401;
-      throw error;
-    }
+  if (userId !== Number(result.user)) {
+    const error = new Error(`ONLY_THE_AUTHOR_CAN_EDIT`);
+    error.status = 401;
+    throw error;
   }
-}
+
+  return result;
+};
 
 // 댓글 내용 또는 공개처리에 대한 수정시
 const updateComment = async (
@@ -75,23 +62,20 @@ const updateComment = async (
   commentId: number,
   commentInfo: CommentDto
 ): Promise<void> => {
-  // 원래 댓글 데이터 유효성검사
-  const db = new ValidateOriginComment(userId, commentId);
-  const origin = await db.originComment();
-  await db.checkCommentWriter();
+  const originComment = await validateComment(userId, commentId);
 
   // commentInfo에서 내용 생략시, 원문 내용으로 채우기
   if (commentInfo.is_private === undefined) {
-    commentInfo.is_private = origin.is_private;
+    commentInfo.is_private = originComment.is_private;
   }
   if (commentInfo.comment === undefined) {
-    commentInfo.comment = origin.comment;
+    commentInfo.comment = originComment.comment;
   }
 
   // 변경사항이 없을 때 에러반환
   if (
-    commentInfo.comment === origin.comment &&
-    commentInfo.is_private === origin.is_private
+    commentInfo.comment === originComment.comment &&
+    commentInfo.is_private === originComment.is_private
   ) {
     const error = new Error(`COMMENT_IS_NOT_CHANGED`);
     error.status = 405;
@@ -103,9 +87,7 @@ const updateComment = async (
 
 const deleteComment = async (commentId: number, userId: number) => {
   // 원댓글 유효성 검사
-  const db = new ValidateOriginComment(userId, commentId);
-  await db.originComment();
-  await db.checkCommentWriter();
+  await validateComment(userId, commentId);
 
   await CommentRepository.softDelete(commentId);
 
