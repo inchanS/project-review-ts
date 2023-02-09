86660c5bafe276b4deb02aa2d62b5358a9ae0b25
c0dject <creseeds@gmail.com>
2023-02-10 03:27:41 +0900

    Add: validateComment - test code 	- with chatGPT

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -107,4 +107,5 @@ export default {
   updateComment,
   getCommentsById,
   deleteComment,
+  validateComment,
 };
--- src/tests/comment.test.ts
+++ src/tests/comment.test.ts
@@ -38,66 +38,103 @@ describe('comment.service UNIT test', () => {
   });
 
   // createComment Unit test
-  test('createComment - Success', async () => {
-    const commentInfo: CommentDto = {
-      user: 1,
-      feed: 1,
-      comment: 'test comment',
-      is_private: false,
-    };
+  describe('createComment', () => {
+    test('Successful creation of a comment', async () => {
+      const commentInfo: CommentDto = {
+        user: 1,
+        feed: 1,
+        comment: 'test comment',
+        is_private: false,
+      };
 
-    jest.spyOn(CommentRepository, 'createComment').mockResolvedValue();
-    await commentsService.createComment(commentInfo);
-    expect(CommentRepository.createComment).toBeCalledTimes(1);
-  });
+      jest.spyOn(CommentRepository, 'createComment').mockResolvedValue();
+      await commentsService.createComment(commentInfo);
+      expect(CommentRepository.createComment).toBeCalledTimes(1);
+    });
 
-  test('createComment - Fail: without comment', async () => {
-    const commentInfo: any = { user: 1, feed: 1, is_private: false };
+    test('Failure: missing comment', async () => {
+      const commentInfo: any = { user: 1, feed: 1, is_private: false };
 
-    await expect(
-      commentsService.createComment(commentInfo)
-    ).rejects.toMatchObject({
-      message: {
-        isNotEmpty: 'comment should not be empty',
-      },
+      await expect(
+        commentsService.createComment(commentInfo)
+      ).rejects.toMatchObject({
+        message: {
+          isNotEmpty: 'comment should not be empty',
+        },
+      });
+    });
+
+    test('Failure: missing feedId', async () => {
+      const commentInfo: any = {
+        user: 1,
+        comment: 'test comment',
+        is_private: false,
+      };
+
+      await expect(
+        commentsService.createComment(commentInfo)
+      ).rejects.toMatchObject({
+        message: {
+          isNotEmpty: 'feed should not be empty',
+        },
+      });
     });
   });
 
-  test('createComment - Fail: without feedId', async () => {
-    const commentInfo: any = {
-      user: 1,
-      comment: 'test comment',
-      is_private: false,
-    };
+  // validateComment Unit test
+  describe('validateComment', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-    await expect(
-      commentsService.createComment(commentInfo)
-    ).rejects.toMatchObject({
-      message: {
-        isNotEmpty: 'feed should not be empty',
-      },
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should throw error if comment does not exist', async () => {
+      jest.spyOn(CommentRepository, 'findOne').mockResolvedValue(null);
+      await expect(commentsService.validateComment(1, 1)).rejects.toThrowError(
+        'ID_1_COMMENT_DOES_NOT_EXIST'
+      );
+    });
+    it('should throw error if user is not the author', async () => {
+      const result: any = { id: 1, user: 2 };
+      jest.spyOn(CommentRepository, 'findOne').mockResolvedValue(result);
+
+      await expect(commentsService.validateComment(1, 1)).rejects.toThrowError(
+        'ONLY_THE_AUTHOR_CAN_EDIT'
+      );
+    });
+
+    it('should return comment if validation is successful', async () => {
+      const result: any = { id: 1, user: 1 };
+      jest.spyOn(CommentRepository, 'findOne').mockResolvedValue(result);
+      const comment = await commentsService.validateComment(1, 1);
+      expect(comment).toEqual({ id: 1, user: 1 });
     });
   });
 
   // updateComment Unit test
-  test('updateComment - Success', async () => {
-    // FIXME : validateComment class 추가된 commentService 변경내역 반영하기
-    const userId = 1;
-    const commentId = 1;
-    const commentInfo: CommentDto = {
-      comment: 'update comment',
-      is_private: false,
-    };
+  describe('updateComment', () => {
+    test('Successful update of a comment', async () => {
+      // FIXME : validateComment class 추가된 commentService 변경내역 반영하기
+      const userId = 1;
+      const commentId = 1;
+      const commentInfo: CommentDto = {
+        comment: 'update comment',
+        is_private: false,
+      };
 
-    const originComment: any = {
-      user: { id: 1 },
-      comment: 'origin comment',
-      is_private: false,
-    };
-    jest.spyOn(CommentRepository, 'findOne').mockResolvedValue(originComment);
-    jest.spyOn(CommentRepository, 'updateComment').mockResolvedValue();
+      const originComment: any = {
+        user: { id: 1 },
+        comment: 'origin comment',
+        is_private: false,
+      };
+      jest.spyOn(CommentRepository, 'findOne').mockResolvedValue(originComment);
+      jest.spyOn(CommentRepository, 'updateComment').mockResolvedValue();
 
-    await commentsService.updateComment(userId, commentId, commentInfo);
-    expect(CommentRepository.updateComment).toBeCalledTimes(1);
+      await commentsService.updateComment(userId, commentId, commentInfo);
+      expect(CommentRepository.updateComment).toBeCalledTimes(1);
+    });
   });
 });
