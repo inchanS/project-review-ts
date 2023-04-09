50f249c6020967ba9580ddbcd7a6b4846ac62fd7
inchanS <song@inchan.dev>
2023-04-10 07:21:35 +0900

    Modify: search API-content 검색어   - feed.content에서 검색어 앞 20글자를 포함하도록 수정   - 검색어 앞 20글자가 없을 때는 content의 시작부터 포함

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -7,7 +7,7 @@ const searchContent = async (query: string) => {
     .select([
       'feed.id',
       'feed.title',
-      `IF(LOWER(feed.content) LIKE LOWER(:query), SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) ), ${
+      `IF(LOWER(feed.content) LIKE LOWER(:query), SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${snippetLength} ), ${
         snippetLength * 2 + query.length
       }), "") AS contentSnippet`,
     ])
