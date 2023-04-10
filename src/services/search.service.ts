e3848deeef32f997d9d86196102e360f81923775
inchanS <song@inchan.dev>
2023-04-10 09:05:59 +0900

    Modify: search API-content 검색어   - feed.content뿐만 아니라 feed.title에서도 검색어 부근만 반환되도록 수정   - 검색어 부근이 반환될 때, 원래의 데이터에서 잘려지는 부분이 있다면 앞뒤로 "..."를 붙여서 축약을 의미할 수 있도록 반환

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -6,10 +6,24 @@ const searchContent = async (query: string) => {
   const result = await FeedRepository.createQueryBuilder('feed')
     .select([
       'feed.id',
-      'feed.title',
-      `IF(LOWER(feed.content) LIKE LOWER(:query), SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${snippetLength} ), ${
+      `IF(LOWER(feed.title) LIKE LOWER(:query), CONCAT(
+        IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${snippetLength}) > 1, '...', ''),
+        SUBSTRING(feed.title, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${snippetLength} ), ${
         snippetLength * 2 + query.length
-      }), "") AS contentSnippet`,
+      }),
+        IF(LENGTH(feed.title) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${snippetLength}) + ${
+        snippetLength * 2 + query.length
+      }), '...', '')
+      ), null) AS titleSnippet`,
+      `IF(LOWER(feed.content) LIKE LOWER(:query), CONCAT(
+        IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${snippetLength}) > 1, '...', ''),
+        SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${snippetLength} ), ${
+        snippetLength * 2 + query.length
+      }),
+        IF(LENGTH(feed.content) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${snippetLength}) + ${
+        snippetLength * 2 + query.length
+      }), '...', '')
+      ), null) AS contentSnippet`,
     ])
     .where('feed.statusId = 1')
     .andWhere('feed.content LIKE :query', { query })
