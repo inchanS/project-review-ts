d9bc293c90e4bca26dbb7fe017eaf3e47a435893
inchanS <song@inchan.dev>
2023-04-15 02:19:38 +0900

    Modify: searchContent API   - 검색어가 제목에 없을 때 원제목을 처음부터 titleSnippet(현재 10으로 설정)만큼만 반환   - titleSnippet보다 제목이 길 때, 끝에 ...으로 축약

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -26,22 +26,36 @@ const searchContent = async (query: string, index: number, limit: number) => {
       'feed.posted_at AS postedAt',
       `IF(LOWER(feed.title) LIKE LOWER(:query), CONCAT(
         IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) > 1, '...', ''),
+        
         SUBSTRING(feed.title, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength} ), ${
         titleSnippetLength * 2 + query.length
       }),
+        
         IF(LENGTH(feed.title) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) + ${
         titleSnippetLength * 2 + query.length
       }), '...', '')
-      ), null) AS titleSnippet`,
+      ), 
+        CONCAT(
+        SUBSTRING(feed.title, 1, ${titleSnippetLength * 2 + query.length}),
+        IF(LENGTH(feed.title) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) + ${
+        titleSnippetLength * 2 + query.length
+      }), '...', '')
+        )
+      ) AS titleSnippet`,
+
       `IF(LOWER(feed.content) LIKE LOWER(:query), CONCAT(
         IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength}) > 1, '...', ''),
+      
         SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength} ), ${
         contentSnippetLength * 2 + query.length
       }),
+      
         IF(LENGTH(feed.content) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength}) + ${
         contentSnippetLength * 2 + query.length
       }), '...', '')
-      ), null) AS contentSnippet`,
+      ), 
+      null
+      ) AS contentSnippet`,
     ])
     .where(
       new Brackets(qb => {
