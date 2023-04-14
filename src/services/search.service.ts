7292c7f9c7e42168d0aac66aa1b7b1402350011b
Song Inchan <song@inchan.dev>
2023-04-15 02:29:18 +0900

    v1.1.4
    Modify: searchContent API 
      - 검색어가 제목에 없을 때 원제목을 처음부터 titleSnippet(현재 10으로 설정)만큼만 반환
      - titleSnippet보다 제목이 길 때, 끝에 ...으로 축약

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -19,29 +19,42 @@ const searchContent = async (query: string, index: number, limit: number) => {
   const titleSnippetLength = 10;
   const contentSnippetLength = 20;
 
-  // FIXME title에 검색어가 없을 때, null이 아닌 앞에서부터 titleSnippetLength만큼의 문자열을 가져오도록 수정
   const result = await FeedRepository.createQueryBuilder('feed')
     .select([
       'feed.id AS id',
       'feed.posted_at AS postedAt',
       `IF(LOWER(feed.title) LIKE LOWER(:query), CONCAT(
         IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) > 1, '...', ''),
+        
         SUBSTRING(feed.title, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength} ), ${
         titleSnippetLength * 2 + query.length
       }),
+        
         IF(LENGTH(feed.title) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) + ${
         titleSnippetLength * 2 + query.length
       }), '...', '')
-      ), null) AS titleSnippet`,
+      ), 
+        CONCAT(
+        SUBSTRING(feed.title, 1, ${titleSnippetLength * 2 + query.length}),
+        IF(LENGTH(feed.title) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.title)) - ${titleSnippetLength}) + ${
+        titleSnippetLength * 2 + query.length
+      }), '...', '')
+        )
+      ) AS titleSnippet`,
+
       `IF(LOWER(feed.content) LIKE LOWER(:query), CONCAT(
         IF(GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength}) > 1, '...', ''),
+      
         SUBSTRING(feed.content, GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength} ), ${
         contentSnippetLength * 2 + query.length
       }),
+      
         IF(LENGTH(feed.content) > (GREATEST(1, LOCATE(LOWER(:originQuery), LOWER(feed.content)) - ${contentSnippetLength}) + ${
         contentSnippetLength * 2 + query.length
       }), '...', '')
-      ), null) AS contentSnippet`,
+      ), 
+      null
+      ) AS contentSnippet`,
     ])
     .where(
       new Brackets(qb => {
