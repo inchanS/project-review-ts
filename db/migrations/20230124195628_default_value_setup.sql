dbdfbc5cb721f704d97ed997e3baa501351dfb5e
inchanS <song@inchan.dev>
2023-04-29 08:19:18 +0900

    Modify: category API 내역 추가   - 생활용품 추가

--- db/migrations/20230124195628_default_value_setup.sql
+++ db/migrations/20230124195628_default_value_setup.sql
@@ -13,10 +13,11 @@ INSERT INTO symbol (id, symbol) VALUES (2, 'I have this too');
 INSERT INTO categories(id, category, description) VALUES (1, '음식', '식당, 카페, 디저트 등');
 INSERT INTO categories(id, category, description) VALUES (2, '전자제품', '스마트폰, 컴퓨터, 가전제품 등');
 INSERT INTO categories(id, category, description) VALUES (3, '의류 및 악세서리', '패션, 의류, 신발, 가방, 액세서리 등');
-INSERT INTO categories(id, category, description) VALUES (4, '화장품', '화장품, 향수, 뷰티 등');
-INSERT INTO categories(id, category, description) VALUES (5, '컨텐츠', '영화, 드라마, 음악, 책, 만화, 이벤트 등');
-INSERT INTO categories(id, category, description) VALUES (6, '여행', '여행, 관광지, 호텔, 숙박시설 등');
-INSERT INTO categories(id, category, description) VALUES (7, '기타', '기타');
+INSERT INTO categories(id, category, description) VALUES (4, '생활용품', '가구, 주방용품, 화장품, 잡화 등');
+INSERT INTO categories(id, category, description) VALUES (5, '화장품', '화장품, 향수, 뷰티 등');
+INSERT INTO categories(id, category, description) VALUES (6, '컨텐츠', '영화, 드라마, 음악, 책, 만화, 이벤트 등');
+INSERT INTO categories(id, category, description) VALUES (7, '여행', '여행, 관광지, 호텔, 숙박시설 등');
+INSERT INTO categories(id, category, description) VALUES (8, '기타', '기타');
 
 
 
