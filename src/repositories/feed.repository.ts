44876241afcd1b44d86903235c0d529d81be78bd
inchanS <song@inchan.dev>
2024-01-24 09:21:44 +0900

    🔨Fix(feed.repository.ts): createFeed 메소드 개선  - 매 저장이나 업데이트 마다 새로운 트랜잭션을 트리거하는 것을 방지  - 이로 인해 발생 가능한 이중 트랜잭션으로 인한 롤백 오류 방지  - 피드 생성 및 저장을 위해 QueryRunner 매니저를 일관성있게 사용

--- src/repositories/feed.repository.ts
+++ src/repositories/feed.repository.ts
@@ -10,23 +10,21 @@ export class FeedRepository extends Repository<Feed> {
     super(Feed, dataSource.createEntityManager());
   }
 
-  async createFeed(feedInfo: Feed, queryRunner?: QueryRunner) {
-    // typeORM의 save, update 등의 메소드는 호출할때마다 새로운 트랜잭션을 시작한다.
+  async createFeed(feedInfo: Feed, queryRunner: QueryRunner) {
+    // typeORM의 save, update 등의 메소드는 호출할때마다 새로운 트랜잭션을 자체적으로 시작한다.
 
     // 때문에 queryRunner를 사용하게 될 때에는 이중 트랜잭션으로 인한 롤백 에러를 방지하기 위해,
     // 다른 방법으로 처리해준다.
-    if (queryRunner) {
-      const feed = queryRunner.manager.create(Feed, feedInfo);
-      await queryRunner.manager.save(Feed, feed);
-    } else {
-      const feed = this.create(feedInfo);
-      await this.save(feed);
-    }
-    return await this.findOne({
+    const feed = queryRunner.manager.create(Feed, feedInfo);
+    await queryRunner.manager.save(feed);
+
+    const result = await queryRunner.manager.findOne(Feed, {
       loadRelationIds: true,
       where: { user: { id: feedInfo.user.id } },
       order: { id: 'DESC' },
     });
+
+    return result;
   }
 
   async updateFeed(feedId: number, feedInfo: Feed) {
