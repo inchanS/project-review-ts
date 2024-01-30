75740c9203388998a73619996158e9284e3e90fb
Song Inchan <song@inchan.dev>
2024-01-30 10:50:23 +0900

    Swagger update (#63)
    * 🧠Refactor: swaggerUI route 함수 이동
     - app.ts -> index.route.ts
    
    * 🍎Chore: remove package reflect-metadata
     - 사용되지 않는 패키지 삭제
    
    * 🧠Refactor(swagger.ts): 서버 선택시 AWS 서버를 디폴트로 수정

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -10,6 +10,8 @@ import uploadRoute from './upload.route';
 import symbolsRoute from './symbols.route';
 import searchRoute from './search.route';
 import testRoute from './test.route';
+import swaggerUi from 'swagger-ui-express';
+import { specs, swaggerOptions } from '../utils/swagger';
 
 router.use('/users', usersRoute);
 router.use('/feeds', feedsRoute);
@@ -19,6 +21,11 @@ router.use('/symbols', symbolsRoute);
 router.use('/upload', uploadRoute);
 router.use('/search', searchRoute);
 router.use('/test', testRoute);
+router.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, swaggerOptions)
+);
 
 // 에러핸들러
 router.use(notFoundHandler);
