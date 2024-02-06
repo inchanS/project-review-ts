83dc8622cd72f1523c1f09f969f3637813b75e25
inchanS <song@inchan.dev>
2024-02-06 20:23:10 +0900

    🔧Modify(DataSource): dateStrings true => false

--- src/repositories/data-source.ts
+++ src/repositories/data-source.ts
@@ -19,7 +19,6 @@ const dataSource = new DataSource({
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  dateStrings: true,
   logging: Boolean(process.env.TYPEORM_LOGGING),
   synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
   charset: 'utf8mb4',
