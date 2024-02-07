46818e24e506839c0098dfb000ccff6d2d152a9e
inchanS <song@inchan.dev>
2024-02-07 16:55:17 +0900

    🔧Modify(feedStatus.entity): is_status 컬럼에 Unique 인덱스 추가

--- src/entities/feedStatus.entity.ts
+++ src/entities/feedStatus.entity.ts
@@ -4,7 +4,7 @@ import { Feed } from './feed.entity';
 
 export type feedStatusType = 'published' | 'temporary' | 'deleted';
 
-// FIXME 왜 이 엔티티 안쓰고 있지? 이거 안쓰고 deleted_at으로 삭제여부 판단하고 있음 - mysql에서 이렇게 찾으면 성능이 안좋을텐데
+// TODO 현재 이 엔티티가 아닌 deleted_at으로 삭제여부 판단하고 있음 - mysql에서 이렇게 찾으면 성능이 안좋을텐데
 //   posted_at이나 deleted_at 컬럼에 인덱스를 걸어주는게 좋을까?
 @Entity('feed_status')
 export class FeedStatus extends Base {
@@ -12,6 +12,7 @@ export class FeedStatus extends Base {
     type: 'enum',
     enum: ['published', 'temporary', 'deleted'],
     default: 'temporary',
+    unique: true,
   })
   is_status?: feedStatusType;
 
