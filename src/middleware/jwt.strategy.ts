659b5174e838d93b5903980ccc888658a50744c7
inchanS <song@inchan.dev>
2024-02-29 04:58:23 +0900

    refactor(TokenDecoder): decodeToken 함수를 정적(static) 메소드로 변경  - 상태 독립성, 편의성, 자원(메모리) 사용 최소화 기대  - feat: Token 에러 핸들링 추가

--- src/middleware/jwt.strategy.ts
+++ src/middleware/jwt.strategy.ts
@@ -1,26 +1,28 @@
 import { NextFunction, Request } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { ValidatorService } from '../services/users/validator.service';
+import { CustomError } from '../utils/util';
 
 class TokenDecoder {
-  private token: string;
+  public static async decodeToken(token: string): Promise<JwtPayload | string> {
+    try {
+      token = token.includes('Bearer')
+        ? token.replace(/^Bearer\s+/, '')
+        : token;
 
-  constructor(token: string) {
-    this.token = token;
-  }
-
-  public async decodeToken(): Promise<{ id: undefined } | JwtPayload | string> {
-    if (!this.token) {
-      return { id: undefined };
+      return jwt.verify(token, process.env.SECRET_KEY || 'MISSING_SECRET_KEY');
+    } catch (err) {
+      if (err instanceof jwt.JsonWebTokenError) {
+        throw new CustomError(401, 'INVALID_TOKEN');
+      } else if (err instanceof jwt.TokenExpiredError) {
+        throw new CustomError(401, 'TOKEN_HAS_EXPIRED');
+      } else {
+        throw new CustomError(
+          401,
+          'AN_ERROR_OCCURRED_WHILE_DECODING_THE_TOKEN'
+        );
+      }
     }
-
-    this.token = this.token.includes('Bearer')
-      ? this.token.replace(/^Bearer\s+/, '')
-      : this.token;
-    return jwt.verify(
-      this.token,
-      process.env.SECRET_KEY || 'MISSING_SECRET_KEY'
-    );
   }
 }
 
@@ -38,54 +40,62 @@ class AuthValidator {
   // 토큰이 없으면 에러를 반환하는 유효성 검사 함수
   public async validateOrReject(): Promise<void> {
     try {
-      const decoder: TokenDecoder = new TokenDecoder(
-        this.req.headers.authorization as string
-      );
-      const decodedToken: { id: undefined } | JwtPayload | string =
-        await decoder.decodeToken();
+      const token: string | undefined = this.req.headers.authorization;
 
-      if (
-        typeof decodedToken === 'object' &&
-        'id' in decodedToken &&
-        decodedToken.id === 'number'
-      ) {
-        // decodedToken.id가 현재 시점에도 유효한지 검사하는 로직
-        await this.validatorService.validateUserInfo(decodedToken.id);
-
-        this.req.userInfo = { id: decodedToken.id };
+      if (!token) {
+        throw new CustomError(401, 'NOT_FOUND_AUTHORIZATION');
       } else {
-        this.next({ status: 401, message: 'INVALID_TOKEN' });
+        const decodedToken: JwtPayload | string =
+          await TokenDecoder.decodeToken(token);
+
+        if (
+          typeof decodedToken === 'object' &&
+          'id' in decodedToken &&
+          decodedToken.id === 'number'
+        ) {
+          // decodedToken.id가 현재 시점에도 유효한지 검사하는 로직
+          await this.validatorService.validateUserInfo(decodedToken.id);
+
+          this.req.userInfo = { id: decodedToken.id };
+          this.next();
+        } else {
+          throw new CustomError(401, 'INVALID_TOKEN_STRUCTURE');
+        }
       }
     } catch (err) {
       this.next(err);
     }
-    this.next();
   }
 
   // 토큰이 없어도 다음으로 넘어가는 유효성 검사 함수
   public async validateOrNext(): Promise<void> {
     try {
-      const decoder: TokenDecoder = new TokenDecoder(
-        this.req.headers.authorization as string
-      );
+      const token: string | undefined = this.req.headers.authorization;
 
-      const decodedToken: { id: undefined } | JwtPayload | string =
-        await decoder.decodeToken();
+      if (!token) {
+        this.next();
+      } else {
+        const decodedToken: JwtPayload | string =
+          await TokenDecoder.decodeToken(token);
 
-      if (
-        typeof decodedToken === 'object' &&
-        decodedToken.hasOwnProperty('id') &&
-        decodedToken.id !== undefined
-      ) {
-        // decodedToken.id가 현재 시점에도 유효한지 검사하는 로직
-        await this.validatorService.validateUserInfo(decodedToken.id);
+        if (
+          typeof decodedToken === 'object' &&
+          decodedToken.hasOwnProperty('id') &&
+          decodedToken.id === 'number'
+        ) {
+          // decodedToken.id가 현재 시점에도 유효한지 검사하는 로직
+          const userId: number = decodedToken.id;
+          await this.validatorService.validateUserInfo(userId);
 
-        this.req.userInfo = { id: decodedToken.id };
+          this.req.userInfo = { id: userId };
+        } else {
+          throw new CustomError(401, 'INVALID_TOKEN_STRUCTURE');
+        }
+        this.next();
       }
     } catch (err) {
       this.next(err);
     }
-    this.next();
   }
 }
 export async function authValidateOrReject(
