4bd25b8c5b2002f31f39bab5ec9809f6344ed931
inchanS <song@inchan.dev>
2024-04-30 11:34:03 +0900

    refactor: update TestInitializer class for dynamic descriptions
    - TestInitializer 클래스의 메서드를 동적 설명으로 업데이트
    - 각 테스트의 초기화 및 종료 시점에 동적으로 설명을 출력하도록 변경
    - 이에 따른 console.log 메시지 업데이트

--- src/tests/integrationTest/testUtils/testInitializer.ts
+++ src/tests/integrationTest/testUtils/testInitializer.ts
@@ -4,29 +4,27 @@ import { TestUtils } from './testUtils';
 export class TestInitializer {
   static initialize(descr: string, tests: () => void) {
     describe(descr, () => {
-      beforeAll(TestInitializer.iniDataSource);
-      afterAll(TestInitializer.clearAndDestroyDataSource);
+      beforeAll(() => TestInitializer.iniDataSource(descr));
+      afterAll(() => TestInitializer.clearAndDestroyDataSource(descr));
       tests();
     });
   }
 
-  private static async iniDataSource() {
+  private static async iniDataSource(descr: string) {
     try {
       await dataSource.initialize();
 
       if (process.env.NODE_ENV === 'test') {
-        console.log(
-          '💥TEST Data Source for Feed CRUD API has been initialized!'
-        );
+        console.log(`💥TEST Data Source for ${descr} has been initialized!`);
       }
     } catch (error) {
-      console.log('Data Source for Feed CRUD API Initializing failed:', error);
+      console.log(`Data Source for ${descr} Initializing failed:`, error);
     }
   }
 
-  private static async clearAndDestroyDataSource() {
+  private static async clearAndDestroyDataSource(descr: string) {
     await TestUtils.clearDatabaseTables(dataSource);
     await dataSource.destroy();
-    console.log('💥TEST Data Source for Feed CRUD API has been destroyed!');
+    console.log(`💥TEST Data Source for ${descr} has been destroyed!`);
   }
 }
