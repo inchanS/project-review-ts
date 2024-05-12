1339ca81ce239816a578c6c14c3aa62335e393db
inchanS <song@inchan.dev>
2024-05-13 06:27:54 +0900

    test: Enhance feedList integration tests to include categories - feedList 통합 테스트를 개선하여, 카테고리별 피드를 검증할 수 있도록 변경 - 각 카테고리별 피드 수를 변경하여, 다양한 테스트 케이스를 검증 - 각 카테고리별 페이징 처리가 제대로 이루어지는지 검증하는 테스트도 추가

--- src/tests/integrationTest/feedList.api.test.ts
+++ src/tests/integrationTest/feedList.api.test.ts
@@ -32,24 +32,51 @@ TestInitializer.initialize('FeedList API test', () => {
     const uploadFiles: UploadFiles[] = [...user1TextFiles, ...user1ImageFiles];
 
     // test feeds
-    const numberOfFeedsPerUser: number[] = [5, 4, 3];
-    const numberOfTotalFeeds: number = numberOfFeedsPerUser.reduce(
-      (acc, value) => {
-        return acc + value;
-      }
-    );
-    let feedInitialUserId: number = 1;
+    const numberOfCategory1FeedsPerUser: number[] = [5, 4, 3];
+    const numberOfCategory2FeedsPerUser: number[] = [3, 2];
+    const numberOfCategory3FeedsPerUser: number[] = [2, 1];
 
-    const feeds: Feed[] = numberOfFeedsPerUser.reduce(
+    let category1FeedInitialId: number = 1;
+    const category1Feeds: Feed[] = numberOfCategory1FeedsPerUser.reduce(
       (prev: Feed[], feedCount: number, index: number) => {
         const feeds: Feed[] = new MakeTestClass(
           users[index].id
-        ).generateMultipleFeeds(feedCount, feedInitialUserId);
-        feedInitialUserId += feedCount;
+        ).generateMultipleFeeds(feedCount, category1FeedInitialId, 1);
+        category1FeedInitialId += feedCount;
         return prev.concat(feeds);
       },
       []
     );
+
+    let category2FeedInitialId: number = category1Feeds.length + 1;
+    const category2Feeds: Feed[] = numberOfCategory2FeedsPerUser.reduce(
+      (prev: Feed[], feedCount: number, index: number) => {
+        const feeds: Feed[] = new MakeTestClass(
+          users[index].id
+        ).generateMultipleFeeds(feedCount, category2FeedInitialId, 2);
+        category2FeedInitialId += feedCount;
+        return prev.concat(feeds);
+      },
+      []
+    );
+
+    let category3FeedInitialId: number = category2FeedInitialId + 1;
+    const category3Feeds: Feed[] = numberOfCategory3FeedsPerUser.reduce(
+      (prev: Feed[], feedCount: number, index: number) => {
+        const feeds: Feed[] = new MakeTestClass(
+          users[index].id
+        ).generateMultipleFeeds(feedCount, category3FeedInitialId, 3);
+        category3FeedInitialId += feedCount;
+        return prev.concat(feeds);
+      },
+      []
+    );
+
+    const allFeeds: Feed[] = [
+      ...category1Feeds,
+      ...category2Feeds,
+      ...category3Feeds,
+    ];
 
     // feedSymbols
     const numberOfFeedSymbolsPerFeed: number[] = [5, 3];
@@ -75,49 +102,84 @@ TestInitializer.initialize('FeedList API test', () => {
     beforeAll(async () => {
       await dataSource.transaction(async transactionalEntityManager => {
         await transactionalEntityManager.save(User, users);
-        await transactionalEntityManager.save(Feed, feeds);
+        await transactionalEntityManager.save(Feed, allFeeds);
         await transactionalEntityManager.save(UploadFiles, uploadFiles);
         await transactionalEntityManager.save(FeedSymbol, feedSymbols);
       });
     });
 
     const endpoint: string = '/feeds/post';
+    const paginationDefaultLimit: number = 10;
 
-    const testFeedList = async (pagination?: Pagination) => {
-      const response: Response = await request(app).get(
-        `${endpoint}${
-          pagination
-            ? `?index=${pagination.startIndex}&limit=${pagination.limit}`
-            : ''
-        }`
-      );
-      const result = response.body;
-      const expectedLength: number =
-        pagination && pagination.limit > numberOfTotalFeeds
-          ? numberOfTotalFeeds
-          : pagination
-          ? pagination.limit
-          : 10;
+    describe('verify length of feedList', () => {
+      const testFeedList = async (pagination?: Pagination) => {
+        const response: Response = await request(app).get(
+          `${endpoint}${
+            pagination
+              ? `?index=${pagination.startIndex}&limit=${pagination.limit}`
+              : ''
+          }`
+        );
+        const result = response.body;
+        const expectedLength: number =
+          pagination && pagination.limit > allFeeds.length
+            ? allFeeds.length
+            : pagination
+            ? pagination.limit
+            : paginationDefaultLimit;
 
-      expect(Array.isArray(result)).toBe(true);
-      expect(result).toHaveLength(expectedLength);
-    };
+        expect(Array.isArray(result)).toBe(true);
+        expect(result).toHaveLength(expectedLength);
+      };
 
-    describe('get feedList', () => {
-      test('default API', async () => {
-        await testFeedList();
+      describe('get feedList', () => {
+        test('default API', async () => {
+          await testFeedList();
+        });
+
+        const testQueryStrings: Pagination[] = [
+          { startIndex: 0, limit: 1 },
+          { startIndex: 0, limit: 5 },
+          { startIndex: 0, limit: 12 },
+          { startIndex: 0, limit: 100 },
+        ];
+
+        testQueryStrings.forEach(pagination => {
+          test(`use Query String: index=${pagination.startIndex}&limit=${pagination.limit}`, async () => {
+            await testFeedList(pagination);
+          });
+        });
       });
 
-      const testQueryStrings: Pagination[] = [
-        { startIndex: 0, limit: 1 },
-        { startIndex: 0, limit: 5 },
-        { startIndex: 0, limit: 12 },
-        { startIndex: 0, limit: 100 },
+      const testQueryStringsOfCategories = [
+        null,
+        { category: 0, expect: paginationDefaultLimit },
+        {
+          category: 1,
+          expect: Math.min(paginationDefaultLimit, category1Feeds.length),
+        },
+        {
+          category: 2,
+          expect: Math.min(paginationDefaultLimit, category2Feeds.length),
+        },
+        {
+          category: 3,
+          expect: Math.min(paginationDefaultLimit, category3Feeds.length),
+        },
       ];
 
-      testQueryStrings.forEach(pagination => {
-        test(`use Query String: index=${pagination.startIndex}&limit=${pagination.limit}`, async () => {
-          await testFeedList(pagination);
+      testQueryStringsOfCategories.forEach(sort => {
+        test(`use Query String Category: category=${sort?.category}`, async () => {
+          const response: Response = await request(app).get(
+            `${endpoint}${sort?.category ? `?categoryId=${sort.category}` : ''}`
+          );
+
+          const result = response.body;
+          const expectedLength: number = sort?.expect
+            ? sort.expect
+            : paginationDefaultLimit;
+
+          expect(result).toHaveLength(expectedLength);
         });
       });
     });
